ae346233878de86efed77d81d9077e9ce92e1174
Alexander Berdyshev <berdishew@gmail.com>
2013-09-23 12:07:44 +0300

    base markup

---
+++ build/js/Eve.js
@@ -0,0 +1,214 @@
+(function() {
+  var Eve;
+
+  Eve = (function() {
+
+    Eve.prototype.errorClass = "error";
+
+    Eve.prototype.eventHandlers = {
+      formSubmitClick: function(e) {
+        var form, isValid, self;
+        self = e.data.self;
+        form = $(this).closest("form");
+        isValid = self.validate(form);
+        if (isValid) {
+          self.sendDataAsync(form).done(function(data) {
+            form.find(".form__success").fadeIn(350);
+          });
+        }
+        return false;
+      },
+      hidePopupClick: function(e) {
+        $(".modal, .modal-overlay").fadeOut();
+      },
+      showPopupClick: function(e) {
+        var selector;
+        selector = $(this).data("modalShow");
+        $("" + selector + ", .modal-overlay").fadeIn();
+      }
+    };
+
+    Eve.prototype.attachEvents = function() {
+      var self;
+      self = this;
+      $(".form input[type=submit]").on('click', {
+        self: self
+      }, self.eventHandlers.formSubmitClick);
+      $(".modal-overlay, .modal__close").on('click', {
+        self: self
+      }, self.eventHandlers.hidePopupClick);
+      $("[data-modal-show]").on('click', {
+        self: self
+      }, self.eventHandlers.showPopupClick);
+    };
+
+    Eve.prototype.validate = function(form) {
+      var hasErrors, inputs, self, validateGeneral, validateOnEmail, validateOnEmpty, validateOnPhone;
+      hasErrors = 0;
+      inputs = form.find("[data-validate]");
+      self = this;
+      validateGeneral = function(input, flag) {
+        if (flag) {
+          input["removeClass"](self.errorClass);
+        } else {
+          hasErrors++;
+          input["addClass"](self.errorClass);
+        }
+      };
+      validateOnEmpty = function(input) {
+        var inputLength, length;
+        length = 2;
+        inputLength = input.val().length;
+        validateGeneral(input, inputLength >= length);
+      };
+      validateOnEmail = function(input) {
+        var res;
+        res = /^([a-zA-Z0-9+_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+        validateGeneral(input, res.test(input.val()));
+      };
+      validateOnPhone = function(input) {
+        var regex;
+        regex = /^(\()?\d{3}(\))?(-|\s)?\d{3}(-|\s)\d{4}$/;
+        validateGeneral(input, regex.test(input.val()));
+      };
+      inputs.each(function(i, el) {
+        var $el;
+        $el = $(el);
+        switch ($el.attr("data-validate")) {
+          case "empty":
+            validateOnEmpty($el);
+            break;
+          case "email":
+            validateOnEmail($el);
+            break;
+          case "tel":
+            validateOnPhone($el);
+            break;
+          default:
+            validateOnEmpty($el);
+        }
+      });
+      return hasErrors === 0;
+    };
+
+    /*
+      * ajax sending form data
+      * @param form - form that needs to send, action attr needed
+    */
+
+
+    Eve.prototype.sendDataAsync = function(form, dataType, showStatusText, actionButton) {
+      var data, originText, self, submit, url;
+      if (dataType == null) {
+        dataType = "text";
+      }
+      if (showStatusText == null) {
+        showStatusText = "Sending ...";
+      }
+      if (actionButton == null) {
+        actionButton = "input[type=submit]";
+      }
+      data = form.serialize();
+      url = form.attr("action");
+      submit = form.find(actionButton);
+      originText = submit.val();
+      submit.val(showStatusText);
+      self = this;
+      return $.ajax({
+        "type": "get",
+        "url": url,
+        "data": data,
+        "dataType": dataType,
+        success: function(response) {
+          submit.val(originText);
+        },
+        error: function(err) {
+          console.log(err);
+          submit.val(originText);
+        }
+      });
+    };
+
+    Eve.prototype.initMainSlider = function() {
+      $(".slider").jCarouselLite({
+        visible: 1,
+        start: 0,
+        scroll: 1,
+        speed: 600,
+        auto: false,
+        autoWidth: true,
+        responsive: true,
+        circular: false,
+        btnNext: ".slider__next",
+        btnPrev: ".slider__prev",
+        afterEnd: function() {
+          var index, length;
+          index = $(".slider__in li.active").index();
+          length = $(".slider__in li").length - 1;
+          console.log(index);
+          console.log(length);
+          $(".slider__prev")[index === 0 ? "fadeOut" : "fadeIn"](1);
+          $(".slider__next")[index === length ? "fadeOut" : "fadeIn"](1);
+        }
+      });
+    };
+
+    Eve.prototype.initBrandsSlider = function() {
+      $(".brands-slider").jCarouselLite({
+        visible: 11,
+        start: 0,
+        scroll: 1,
+        auto: true,
+        speed: 300,
+        autoWidth: true,
+        responsive: true
+      });
+    };
+
+    Eve.prototype.initPlaceholder = function() {
+      if (!Modernizr.input.placeholder) {
+        $("[placeholder]").focus(function() {
+          var input;
+          input = $(this);
+          if (input.val() === input.attr("placeholder")) {
+            input.val("");
+            return input.removeClass("placeholder");
+          }
+        }).blur(function() {
+          var input;
+          input = $(this);
+          if (input.val() === "" || input.val() === input.attr("placeholder")) {
+            input.addClass("placeholder");
+            return input.val(input.attr("placeholder"));
+          }
+        }).blur();
+        $("[placeholder]").parents("form").submit(function() {
+          return $(this).find("[placeholder]").each(function() {
+            var input;
+            input = $(this);
+            if (input.val() === input.attr("placeholder")) {
+              return input.val("");
+            }
+          });
+        });
+      }
+    };
+
+    function Eve() {
+      var self;
+      self = this;
+      self.initMainSlider();
+      self.initBrandsSlider();
+      self.attachEvents();
+      self.initPlaceholder();
+    }
+
+    return Eve;
+
+  })();
+
+  $(document).ready(function() {
+    $.fn.Eve = new Eve;
+  });
+
+}).call(this);
---
+++ coffeescripts/Eve.coffee
@@ -0,0 +1,169 @@
+
+class Eve
+  errorClass:"error"
+  eventHandlers:
+    formSubmitClick:(e)->
+      self = e.data.self
+      form = $(@).closest("form")
+      isValid = self.validate(form)
+      if isValid
+        self.sendDataAsync(form).done(
+          (data)->
+            form.find(".form__success").fadeIn(350)
+            return
+        )
+      return false
+    hidePopupClick:(e)->
+      $(".modal, .modal-overlay").fadeOut()
+      return
+    showPopupClick:(e)->
+      selector = $(@).data("modalShow")
+      $("#{selector}, .modal-overlay").fadeIn()
+      return
+  attachEvents: ->
+    self = this
+    $(".form input[type=submit]").on('click',{self:self} , self.eventHandlers.formSubmitClick)
+    $(".modal-overlay, .modal__close").on('click',{self:self} , self.eventHandlers.hidePopupClick)
+    $("[data-modal-show]").on('click',{self:self} , self.eventHandlers.showPopupClick)
+    return
+
+  validate:(form)->
+    hasErrors = 0 # if it equals to 0 => no errors
+    inputs = form.find("[data-validate]");
+    self = @
+
+    validateGeneral = (input,flag)->
+      if flag
+        input["removeClass"](self.errorClass)
+      else
+        hasErrors++;
+        input["addClass"](self.errorClass)
+      return
+
+    validateOnEmpty = (input)->
+      length = 2
+      inputLength = input.val().length;
+      validateGeneral(input,inputLength >= length)
+      return
+
+    validateOnEmail = (input)->
+      res = /^([a-zA-Z0-9+_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+      validateGeneral(input,  res.test(input.val()))
+      return;
+
+    validateOnPhone = (input)->
+      regex = /^(\()?\d{3}(\))?(-|\s)?\d{3}(-|\s)\d{4}$/
+      validateGeneral(input,  regex.test(input.val()))
+      return;
+
+    inputs.each(
+      (i,el)->
+        $el = $(el)
+        switch $el.attr("data-validate")
+          when "empty"
+            validateOnEmpty($el)
+          when "email"
+            validateOnEmail($el)
+          when "tel"
+            validateOnPhone($el)
+          else
+            validateOnEmpty($el)
+        return
+    )
+    return hasErrors is 0
+
+  ###
+  * ajax sending form data
+  * @param form - form that needs to send, action attr needed
+  ###
+  sendDataAsync:(form,dataType="text",showStatusText = "Sending ...",actionButton = "input[type=submit]")->
+    data = form.serialize()
+    url = form.attr("action");
+    submit = form.find(actionButton);
+    originText = submit.val()
+    submit.val(showStatusText)
+    self = @
+    $.ajax(
+      "type": "get"
+      "url":url
+      "data":data
+      "dataType":dataType,
+      success:(response)->
+        submit.val(originText)
+        return
+      error:(err)->
+        console.log err
+        submit.val(originText)
+        return
+    )
+
+  initMainSlider:()->
+    $(".slider").jCarouselLite({
+      visible: 1
+      start: 0
+      scroll:1
+      speed:600
+      auto:off
+      autoWidth:true
+      responsive:true
+      circular:false
+      btnNext: ".slider__next"
+      btnPrev: ".slider__prev"
+      afterEnd:()->
+        index = $(".slider__in li.active").index()
+        length = $(".slider__in li").length - 1
+        console.log index
+        console.log length
+        $(".slider__prev")[if index is 0 then "fadeOut" else "fadeIn"](1)
+        $(".slider__next")[if index is length then "fadeOut" else "fadeIn"](1)
+        return
+    })
+    return
+
+  initBrandsSlider:()->
+    $(".brands-slider").jCarouselLite({
+      visible: 11
+      start: 0
+      scroll:1
+      auto:true
+      speed:300
+      autoWidth:true
+      responsive:true
+    })
+    return
+
+
+  initPlaceholder:()->
+    unless Modernizr.input.placeholder
+      $("[placeholder]").focus(->
+        input = $(this)
+        if input.val() is input.attr("placeholder")
+          input.val ""
+          input.removeClass "placeholder"
+      ).blur(->
+        input = $(this)
+        if input.val() is "" or input.val() is input.attr("placeholder")
+          input.addClass "placeholder"
+          input.val input.attr("placeholder")
+      ).blur()
+      $("[placeholder]").parents("form").submit ->
+        $(this).find("[placeholder]").each ->
+          input = $(this)
+          input.val ""  if input.val() is input.attr("placeholder")
+    return
+
+
+
+  constructor: ()->
+    self = this
+    self.initMainSlider()
+    self.initBrandsSlider()
+    self.attachEvents()
+    self.initPlaceholder()
+
+
+$(document).ready(
+  ()->
+    $.fn.Eve = new Eve
+    return
+)
